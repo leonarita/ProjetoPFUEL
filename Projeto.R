60c03d618702e2dc849830edacb19a5db468e1a1
Leonardo <leo_narita@hotmail.com>
2022-01-12 23:23:36 -0300

    Iniciando projeto R

---
+++ Projeto.R
@@ -0,0 +1,17 @@
+# instalando tidyverse
+install.packages("tidyverse")
+
+# libraries utilizadas
+library(purrr)
+library(ggplot2)
+
+# importando dados
+dados <- read.csv("BRAZIL_CITIES.csv", header = TRUE, sep = ";", fileEncoding = "UTF-8")
+dados
+
+# vendo tipagem dos dados
+str(dados)
+
+
+
+R.Version()
