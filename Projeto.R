f869e1604ae365c0b9a62be09c9995be4c30506a
Leonardo <leo_narita@hotmail.com>
2022-01-20 12:14:23 -0300

    Primeiras tentativas

--- Projeto.R
+++ Projeto.R
@@ -1,17 +1,33 @@
-# instalando tidyverse
+# Instalando pacotes
 install.packages("tidyverse")
+install.packages("dplyr")
 
-# libraries utilizadas
+# Importando libraries utilizadas
 library(purrr)
 library(ggplot2)
+library(dplyr)
 
-# importando dados
+# Importando dados
 dados <- read.csv("BRAZIL_CITIES.csv", header = TRUE, sep = ";", fileEncoding = "UTF-8")
 dados
 
-# vendo tipagem dos dados
+# Vendo tipagem dos dados
 str(dados)
 
+# Conhecendo melhor os dados: visualizando quantas cidades tem em cada estado
+ggplot(data = dados) + geom_bar(mapping = aes(x = STATE))
+dados %>% count(STATE)
 
+# Conhecendo melhor os dados: visualizando a distribuição do IDH
+ggplot(data = dados) + geom_histogram(mapping = aes(x = IDHM), binwidth = 0.1)
+ggplot(data = dados, mapping = aes(x = IDHM)) + geom_histogram(binwidth = 0.1)
+dados %>% count(cut_width(IDHM, 0.1))
+
+# CASO 1
+ggplot(data = dados, mapping = aes(x = IDHM, colour = STATE)) + geom_histogram(binwidth = 0.1)
+
+# CASO 2
+ggplot(data = dados, mapping = aes(x = IDHM, colour = IBGE_RES_POP_ESTR)) + geom_freqpoly(binwidth = 0.1)
+ggplot(data = dados, mapping = aes(x = IBGE_RES_POP, colour = IBGE_RES_POP_ESTR)) + geom_freqpoly(binwidth = 0.8)
 
 
